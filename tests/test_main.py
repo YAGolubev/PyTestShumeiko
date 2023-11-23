afdd38693a01f407be5f6f57511b33e86baf2b14
YAGolubev <yagolubev@inbox.ru>
2023-11-23 08:33:30 +0300

    test Calculator

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,13 +1,25 @@
-from src.main import A
+import pytest
+
+from src.main import Calculator
 
 
-def test_main():
-    assert A.x == 1
+@pytest.mark.parametrize(
+    "x, y, res",
+    [
+        (1, 2, 0.5),
+        (5, -1, -5),
+    ],
+)
+def test_devide(x, y, res):
+    assert Calculator().devide(x, y) == res
 
 
-def test_2():
-    assert 2 == 2
-
-
-def test_3():
-    assert 3 == 3
+@pytest.mark.parametrize(
+    "x, y, res",
+    [
+        (1, 2, 3),
+        (5, -1, 4),
+    ],
+)
+def test_add(x, y, res):
+    assert Calculator().add(x, y) == res
